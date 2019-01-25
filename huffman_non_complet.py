fa917b6109f77953d994cf5464aad389cec58271
jonathan <chabaud.jonathan@gmail.com>
2019-01-25 07:27:21 -0800

    templatecorrection

--- huffman_non_complet.py
+++ huffman_non_complet.py
@@ -1,11 +1,11 @@
 #####################################################
-######  Introduction à la cryptographie  	###
-#####   Codes de Huffman             		###
+#  Introduction à la cryptographie  	###
+#   Codes de Huffman             		###
 ####################################################
 
 from heapq import *
 
-###  distribution de proba sur les letrres
+#  distribution de proba sur les letrres
 
 caracteres = [
     ' ', 'a', 'b', 'c', 'd', 'e', 'f',
@@ -19,6 +19,7 @@ proba = [
     0.0623, 0.0459, 0.0256, 0.0081, 0.0555, 0.0697, 0.0572,
     0.0506, 0.0100, 0.0000, 0.0031, 0.0021, 0.0008  ]
 
+
 def frequences() :
     table = {}
     n = len(caracteres)
@@ -26,58 +27,60 @@ def frequences() :
         table[caracteres[i]] = proba[i]
     return table
 
+
 F = frequences()
 
-###  la classe Arbre
+#  la classe Arbre
+
 
 class Arbre :
     def __init__(self, lettre, gauche=None, droit=None):
         self.gauche=gauche
         self.droit=droit
         self.lettre=lettre
+
     def estFeuille(self):
         return self.gauche == None and self.droit == None
+
     def estVide(self):
         return self == None
+
     def __str__(self):
         return '<'+ str(self.lettre)+'.'+str(self.gauche)+'.'+str(self.droit)+'>'
 
 
-###  Ex.1  construction de l'arbre d'Huffamn utilisant la structure de "tas binaire"
-def arbre_huffman(frequences) :
+#  Ex.1  construction de l'arbre d'Huffamn utilisant la structure de "tas binaire"
+def arbre_huffman(frequences):
     tas = []
     for i in frequences:
-        tas.append((frequences[i], i , Arbre(i)))
+        tas.append((frequences[i], i, Arbre(i)))
     heapify(tas)
-    while len(tas)>1:
+    while len(tas) > 1:
         gauche = heappop(tas)
         droite = heappop(tas)
         New_Arbre = Arbre("", gauche[2], droite[2])
         heappush(tas, (gauche[0]+droite[0], "", New_Arbre))
-    return tas
-    # à compléter
+    return heappop(tas)[2]
 
-###  Ex.2  construction du code d'Huffamn
+#  Ex.2  construction du code d'Huffamn
 
-def parcours(arbre,prefixe,code) :    
+def parcours(arbre,prefixe,code):
     if arbre.estFeuille():
         code[arbre.lettre] = prefixe
     else:
         parcours(arbre.gauche, prefixe+"0", code)
         parcours(arbre.droit, prefixe+"1", code)
 
-def code_huffman(arbre) :
+def code_huffman(arbre):
     # on remplit le dictionnaire du code d'Huffman en parcourant l'arbre
     code = {}
-    parcours(arbre,'',code)
+    parcours(arbre, '', code)
     return code
 
 
+#  Ex.3  encodage d'un texte contenu dans un fichier
 
-
-###  Ex.3  encodage d'un texte contenu dans un fichier
-
-def encodage(dico,fichier) :
+def encodage(dico,fichier):
     # à compléter
     return 0
 
@@ -85,7 +88,7 @@ def encodage(dico,fichier) :
 #print(encode)
 
 
-###  Ex.4  décodage d'un fichier compresse
+#  Ex.4  décodage d'un fichier compresse
 
 def decodage(arbre,fichierCompresse) :
     return 0
@@ -93,8 +96,4 @@ def decodage(arbre,fichierCompresse) :
     return 0
 
 #decode = decodage(H,'leHorlaEncoded.txt')
-<<<<<<< HEAD
 #print(decode)
-=======
-#print(decode)
->>>>>>> 8f981a40edc6c62f7aec45fd8034e73b3decbe9b
