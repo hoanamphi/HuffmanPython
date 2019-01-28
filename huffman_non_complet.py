fedd9fdb148449815dc8f377da6d8a3405b03561
jonathan <chabaud.jonathan@gmail.com>
2019-01-28 02:04:00 -0800

    pushfirstex

--- huffman_non_complet.py
+++ huffman_non_complet.py
@@ -11,19 +11,19 @@ caracteres = [
     ' ', 'a', 'b', 'c', 'd', 'e', 'f',
     'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't',
-    'u', 'v', 'w', 'x', 'y', 'z' ]
+    'u', 'v', 'w', 'x', 'y', 'z']
 
 proba = [
     0.1835, 0.0640, 0.0064, 0.0259, 0.0260, 0.1486, 0.0078,
     0.0083, 0.0061, 0.0591, 0.0023, 0.0001, 0.0465, 0.0245,
     0.0623, 0.0459, 0.0256, 0.0081, 0.0555, 0.0697, 0.0572,
-    0.0506, 0.0100, 0.0000, 0.0031, 0.0021, 0.0008  ]
+    0.0506, 0.0100, 0.0000, 0.0031, 0.0021, 0.0008]
 
 
-def frequences() :
+def frequences():
     table = {}
     n = len(caracteres)
-    for i in range(n) :
+    for i in range(n):
         table[caracteres[i]] = proba[i]
     return table
 
@@ -33,20 +33,23 @@ F = frequences()
 #  la classe Arbre
 
 
-class Arbre :
+class Arbre:
     def __init__(self, lettre, gauche=None, droit=None):
-        self.gauche=gauche
-        self.droit=droit
-        self.lettre=lettre
+        self.gauche = gauche
+        self.droit = droit
+        self.lettre = lettre
 
     def estFeuille(self):
-        return self.gauche == None and self.droit == None
+        return self.gauche is None and self.droit is None
 
     def estVide(self):
-        return self == None
+        return self is None
+
+    def __lt__(self, other):
+        return self.lettre < other.lettre
 
     def __str__(self):
-        return '<'+ str(self.lettre)+'.'+str(self.gauche)+'.'+str(self.droit)+'>'
+        return '<' + str(self.lettre)+'.'+str(self.gauche)+'.'+str(self.droit)+'>'
 
 
 #  Ex.1  construction de l'arbre d'Huffamn utilisant la structure de "tas binaire"
@@ -64,12 +67,14 @@ def arbre_huffman(frequences):
 
 #  Ex.2  construction du code d'Huffamn
 
-def parcours(arbre,prefixe,code):
+
+def parcours(arbre, prefixe, code):
     if arbre.estFeuille():
         code[arbre.lettre] = prefixe
     else:
         parcours(arbre.gauche, prefixe+"0", code)
         parcours(arbre.droit, prefixe+"1", code)
+
 
 def code_huffman(arbre):
     # on remplit le dictionnaire du code d'Huffman en parcourant l'arbre
@@ -80,20 +85,33 @@ def code_huffman(arbre):
 
 #  Ex.3  encodage d'un texte contenu dans un fichier
 
-def encodage(dico,fichier):
-    # à compléter
-    return 0
 
-#encode = encodage(dico,'leHorla.txt')
-#print(encode)
+def encodage(dico, fichier):
+    f = open(fichier, "r")
+    text = f.read()
 
+    for lettre in text:
+        if not(lettre in dico):
+            dico[lettre] = 1/len(text)
+        else:
+            dico[lettre] = dico[lettre]+1/len(text)
+
+    code = code_huffman(arbre_huffman(dico))
+    new_text = ""
+    for lettre in text:
+        new_text += code[lettre]
+
+    bytes = [int(new_text[i:i+7], 2) for i in range(0, len(new_text), 7)]
+
+    print(bytes)
+
+
+
+
+encodage({}, "leHorla.txt.txt")
 
 #  Ex.4  décodage d'un fichier compresse
 
-def decodage(arbre,fichierCompresse) :
-    return 0
-    # à compléter
-    return 0
 
-#decode = decodage(H,'leHorlaEncoded.txt')
-#print(decode)
+def decodage(arbre, fichierCompresse):
+    return 0
